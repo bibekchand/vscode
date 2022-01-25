3237463d22960d363bf95f29dca7b0c65411b1f6
bibekchand <bibekchand843@gmail.com>
2022-01-25 16:23:03 +0545

    commit added

---
+++ app.py
@@ -0,0 +1,25 @@
+for i in range(20):
+    if i<5:
+        print('it is less than five')
+    if i==5:
+        print('it is five')    
+    if i>10:
+        print('it is greater than 10')
+    print(i)
+terobau = ['a', 'b', 'c', 'd' ] 
+terobau.sort()
+#slicing
+print(terobau[::-1])
+print(len(terobau))
+print(max(terobau))
+print(min(terobau))
+# terobau.pop()
+# print(terobau)
+# terobau.pop()ls
+terobau[1]= 'hello'
+print(terobau)
+terobau.append('b') #add the element to the list
+terobau.pop()# removes the element 'last one from the list'
+tp = (1, 2 ,3)
+print(tp)
+tp(1) = 3
